5d90d87481cff078b56781b532a919d5b4a3bf24
Nara Alencar <116104559+naracodes@users.noreply.github.com>
2023-02-07 21:57:48 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,76 @@
+import br.com.ada.polotech925.*;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem Vindo ao Gerenciador de Arquivos");
+        switch (montarOpcoesMenu()){
+            case 1:
+                System.out.println("Digite o caminho: ");
+                String path = scanner.nextLine();
+               int opcao = montarOpcoesFolders();
+               if (opcao == 1){
+                   path += "/reminders/";
+               } else if (opcao == 2) {
+                    path += "/importants/";
+                } else if (opcao == 3) {
+                    path += "";
+                } else if (opcao == 4) {
+                    path += "/images/";
+                }
+                FolderManagement fM = new FolderOrchestrator();
+                fM.createFolder(path);
+                break;
+            case 2:
+                MFile mFile = new MFile();
+                System.out.println("Digite o caminho: ");
+                path = scanner.nextLine();
+                System.out.println("Digite o conteudo: ");
+                String content = scanner.nextLine();
+                System.out.println("Digite o nome do arquivo: ");
+                String name = scanner.nextLine();
+                System.out.println("Digite o tipo do arquivo: ");
+                String type = scanner.nextLine().toUpperCase();
+
+                FileDatabase fO = new FileOrchestrator();
+                fO.saveFile(path, content, type, name);
+            case 7:
+                System.out.println("Digite um caminho: ");
+                path = scanner.nextLine();
+                FolderManagement fM2 = new FolderOrchestrator();
+                fM2.removeFolder(path);
+            default:
+                System.out.println("Opção inválida");
+        }
+
+    }
+
+    public static int montarOpcoesFolders(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ESCOLHA UM DOS TIPOS");
+        System.out.println("1- REMINDER");
+        System.out.println("2- IMPORTANT");
+        System.out.println("3- SIMPLE");
+        System.out.println("4- IMAGE");
+        System.out.println("Digite a opção desejada: ");
+        int opcao = scanner.nextInt();
+        return opcao;
+    }
+
+    public static int montarOpcoesMenu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("----------MENU-----------");
+        System.out.println("1- Criar Pasta ok");
+        System.out.println("2- Criar arquivo txt");
+        System.out.println("3- Salvar imagem");
+        System.out.println("4- Listar arquivos txt");
+        System.out.println("5- Listar imagens");
+        System.out.println("6- Remover arquivos");
+        System.out.println("7- Remover Pasta");
+        System.out.println("Digite a opção desejada: ");
+        int opcao = scanner.nextInt();
+        return opcao;
+    }
+}
