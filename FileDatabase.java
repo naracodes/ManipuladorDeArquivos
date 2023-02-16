23c571437c7d5be5864206b28111b40b445ef94c
Nara Alencar <116104559+naracodes@users.noreply.github.com>
2023-02-16 19:00:44 -0300

    Update FileDatabase.java

--- FileDatabase.java
+++ FileDatabase.java
@@ -1,7 +1,7 @@
 package br.com.ada.polotech925;
 
 public interface FileDatabase {
-    void saveFile(String directory, String content, String type, String nameFile);
+    void saveFile(String directory, String content, MFileAnnotationTypeEnum type, String nameFile);
     void recoveryFile(String directory, String nameFile);
     void removeFile(String directory, String nameFile, String type);
     void listAllFiles(String directory);
